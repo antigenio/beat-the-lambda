2f83f50e37de6df1b53f37a51935295f97b0382d
Douglas <code@douglas.brunner.name>
2018-03-11 20:49:43 -0400

    Added combineStrings function

--- typesandfunc/app/Main.hs
+++ typesandfunc/app/Main.hs
@@ -1,2 +1,8 @@
+sayHi :: String
+sayHi = "Hello World!"
+
+combineStrings :: String -> String -> String
+combineStrings x y = x ++ y
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = putStrLn (combineStrings "Hello " "World!")
